3673a3b36d2a36c91810b37e5b863af8b32d527f
Laura Palmer <cms3688@gmail.com>
2021-05-24 14:00:46 -0400

    threw out password function because it is beyond scope of current project

--- allowance.py
+++ allowance.py
@@ -1,7 +1,6 @@
 import csv
 import os.path
 
-
 def main():
     open_program()
     next_up()
@@ -9,47 +8,43 @@ def main():
 def open_program():
 #This function will check whether a CSV file already exists. If it does, the program will know that initial setup has already been completed. If it does not, tbe program will proceed to initial setup.#
     if os.path.isfile('allowance.csv'):
+        child_name, current_balance = open_csv('allowance.csv')
+        print(child_name, current_balance)
         parent_or_child = input("Are you the parent or child? Type P or C: ")
-        if parent_or_child == "p":
-            current_balance = parent_maintenance()
+        if parent_or_child == "p" or parent_or_child == "P":
+            parent_maintenance()
         else:
             child_maintenance()
     else:
-        name, password, current_balance = initial_setup()
+        name, current_balance = initial_setup()
+    return name, current_balance
 
 def initial_setup():
     #This function will be initiated upon first use of the app only. It will provide the child's name, a starting balance, and a password for the parent to use when making deposits or deductions from the account#
     name = input("What is your child's first name?: ")
-    password = (input("Enter a password: "))
     current_balance = int(input("How much money do you want your child's account to start with?: "))
     with open('allowance.csv', 'w', newline='') as allowance_file:
         writer = csv.writer(allowance_file)
         writer.writerow(['Name', 'Amount'])
         writer.writerow([name, current_balance])
-    return name, password, current_balance
+    return name, current_balance
 
-def parent_maintenance(current_balance, password):
+def parent_maintenance(name, current_balance):
     #This function will be initiated when a parent uses the app and initial setup has already been completed#
-    parent_input = input("Enter password: ")
-    if parent_input == password:
+    parent_question = input("Do you want to add or deduct money from the account? Type A or D: ")
+    if parent_question == "a":
+            add_amount = int(input("How much money do you want to add?: "))
+            current_balance = (add_amount + current_balance)
+            print(current_balance)
+            return current_balance
+    else:
         parent_question = input("Do you want to add or deduct money from the account? Type A or D: ")
         if parent_question == "a":
             add_amount = int(input("How much money do you want to add?: "))
             current_balance = (add_amount + current_balance)
             print(current_balance)
             return current_balance
-    else:
-        parent_input = input("Incorrect password. Please try again: ")
-        if parent_input == password:
-            parent_question = input("Do you want to add or deduct money from the account? Type A or D: ")
-            if parent_question == "a":
-                add_amount = int(input("How much money do you want to add?: "))
-                current_balance = (add_amount + current_balance)
-                print(current_balance)
-                return current_balance
-        else:
-            print("Incorrect password. Exiting app")
-            open_program()
+        open_program()
 
 
 def next_up(current_balance):
@@ -68,6 +63,17 @@ def next_up(current_balance):
     elif choice == "e":
         open_program()
     return current_balance
+
+def open_csv(file_name):
+    with open(file_name, newline='') as f:
+        reader = csv.reader(f)
+        next(reader)
+        current_balance = 0
+        for r in reader:
+            child_name = r[0]
+            current_balance += int(r[1])
+        return child_name, current_balance
+
 
 if __name__ == '__main__':
     main()
