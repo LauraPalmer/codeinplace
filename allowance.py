3f931219679632e3ffd1fd341b8420a8376c9f39
Laura Palmer <cms3688@gmail.com>
2021-05-24 21:25:47 -0400

    fixed problems with varible scope

--- allowance.py
+++ allowance.py
@@ -3,7 +3,6 @@ import os.path
 
 def main():
     open_program()
-    next_up()
 
 def open_program():
 #This function will check whether a CSV file already exists. If it does, the program will know that initial setup has already been completed. If it does not, tbe program will proceed to initial setup.#
@@ -12,12 +11,12 @@ def open_program():
         print(child_name, current_balance)
         parent_or_child = input("Are you the parent or child? Type P or C: ")
         if parent_or_child == "p" or parent_or_child == "P":
-            parent_maintenance()
+            current_balance = parent_maintenance(child_name, current_balance)
         else:
             child_maintenance()
     else:
         name, current_balance = initial_setup()
-    return name, current_balance
+    return child_name, current_balance
 
 def initial_setup():
     #This function will be initiated upon first use of the app only. It will provide the child's name, a starting balance, and a password for the parent to use when making deposits or deductions from the account#
@@ -29,25 +28,24 @@ def initial_setup():
         writer.writerow([name, current_balance])
     return name, current_balance
 
-def parent_maintenance(name, current_balance):
+def parent_maintenance(child_name, current_balance):
     #This function will be initiated when a parent uses the app and initial setup has already been completed#
     parent_question = input("Do you want to add or deduct money from the account? Type A or D: ")
     if parent_question == "a":
             add_amount = int(input("How much money do you want to add?: "))
-            current_balance = (add_amount + current_balance)
+            current_balance = add_amount + current_balance
             print(current_balance)
             return current_balance
     else:
-        parent_question = input("Do you want to add or deduct money from the account? Type A or D: ")
-        if parent_question == "a":
-            add_amount = int(input("How much money do you want to add?: "))
-            current_balance = (add_amount + current_balance)
+        if parent_question == "d":
+            deduct_amount = int(input("How much money do you want to deduct?: "))
+            current_balance = (current_balance - deduct_amount)
             print(current_balance)
             return current_balance
         open_program()
+    return child_name, current_balance
 
-
-def next_up(current_balance):
+def next_up(child_name, current_balance):
     #This function will be initiated after initial setup of the program#
     choice = input("What would you like to do now? You can add money (a), deduct money (d), view balance (v), or exit parental section (e). Enter choice:  ")
     if choice == "a":
